468b28623a4989e16e7e925c752b3bfb355783f2
Lars Kristian Johansen <larskrjo@gmail.com>
2014-06-19 10:46:01 +0200

    initial maven setup

---
+++ src/main/java/net/larskristian/ruter/traffic/App.java
@@ -0,0 +1,13 @@
+package net.larskristian.ruter.traffic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
