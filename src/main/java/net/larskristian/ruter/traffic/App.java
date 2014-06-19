1abf2509444ddf10722d65b9d1232dedbeb3444c
Lars Kristian Johansen <larskrjo@gmail.com>
2014-06-19 12:15:43 +0200

    test

--- src/main/java/net/larskristian/ruter/traffic/App.java
+++ src/main/java/net/larskristian/ruter/traffic/App.java
@@ -6,8 +6,6 @@ package net.larskristian.ruter.traffic;
  */
 public class App {
 
-    String test  = "ubrukelig";
-
     public static void main(String[] args) {
         System.out.println( "Hello World!" );
     }
